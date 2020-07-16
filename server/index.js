e9d09047d1dceb877747fab14787e4cd137af7ba
Guillermo <guillermo.sanz.gonzalez@alumnos.upm.es>
2020-07-16 20:46:20 +0200

    Back to Front End Routing in Production

--- server/index.js
+++ server/index.js
@@ -35,6 +35,20 @@ app.use(passport.session());
 require("./routes/authRoutes")(app); // This is invoking the function exported by authRoutes in the routes directory
 require("./routes/billingRoutes")(app);
 
+// Route handler for the react routes. This will only run on production.
+if (process.env.NODE_ENV === "production") {
+   // Express will serve up production assests like our main.js file, or main.css file!
+   // If we don't have a route handler set up for one thing, then look into the client/build directory and
+   // try to see if there's some file inside of there that matches up with what this request is looking for:
+   app.use(express.static("client/build"));
+
+   // If there's not, then Express will serve up the index.html file
+   const path = require("path");
+   app.get("*", (req, res) => {
+      res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+   });
+}
+
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
    console.log("App listening on port 5000!!");
