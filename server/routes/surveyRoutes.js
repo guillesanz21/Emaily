be8ff4d5161ad2f446a941486a1dd41094290b39
Guillermo <guillermo.sanz.gonzalez@alumnos.upm.es>
2020-07-21 12:22:46 +0200

    Webhooks

--- server/routes/surveyRoutes.js
+++ server/routes/surveyRoutes.js
@@ -1,5 +1,7 @@
+const _ = require("lodash");
+const { Path } = require("path-parser");
+const { URL } = require("url");
 const mongoose = require("mongoose");
-const axios = require("axios"); // DELETE AND UNINSTALL THIS
 const requireLogin = require("../middlewares/requireLogin");
 const requireCredits = require("../middlewares/requireCredits");
 const Mailer = require("../services/Mailer");
@@ -8,6 +10,54 @@ const surveyTemplate = require("../services/emailTemplates/surveyTemplate");
 const Survey = mongoose.model("surveys");
 
 module.exports = (app) => {
+  app.get("/api/surveys/:surveyId/:choice", (req, res) => {
+    res.send("Thanks for voting!");
+  });
+
+  /* { email, url } are 2 params from the event object
+   * new Path:          We create a url whose parameters can be extracted
+   * URL(url).pathname: Extract the relative url from the given event. Example: /api/surveys/5324sa421/yes
+   * p.test():          Assign the survey ID and the choice inside of an object
+   * If the url can match the values, then we obtain: { surveyId: '5324sa421', choice: "yes" }
+   * If not (maybe because it wasn't a click event), then we obtain an undefined object.
+   * _.compact:        Removes the undefined events (the ones which aren't of type click)
+   * _.uniqBy:         Removes the duplicated ones
+   */
+  app.post("/api/surveys/webhooks", (req, res) => {
+    const p = new Path("/api/surveys/:surveyId/:choice");
+    _.chain(req.body)
+      .map(({ email, url }) => {
+        const match = p.test(new URL(url).pathname);
+        if (match) {
+          return { email, surveyId: match.surveyId, choice: match.choice };
+        }
+      })
+      .compact()
+      .uniqBy("email", "surveyId")
+      .each(({ surveyId, email, choice }) => {
+        // Find the survey with the same id as the given one, besides, for every survey from the surveys collections
+        // look through its recipients property, and inside, find the recipient that matches the given email and have
+        // the responded property set to false. Then update the responded value, the num of answers of the survey and
+        // the date of the moment the survey was answered
+        Survey.updateOne(
+          {
+            _id: surveyId,
+            recipients: {
+              $elemMatch: { email: email, responded: false },
+            },
+          },
+          {
+            $inc: { [choice]: 1 }, // $inc === increment the "yes" or the "no" value by 1.
+            $set: { "recipients.$.responded": true }, // Set the responded prop from the element matched to true.
+            lastResponded: new Date(),
+          }
+        ).exec();
+      })
+      .value();
+
+    res.send({});
+  });
+
   app.post("/api/surveys", requireLogin, requireCredits, async (req, res) => {
     const { title, subject, body, recipients } = req.body;
 
@@ -37,25 +87,6 @@ module.exports = (app) => {
       res.send(user);
     } catch (err) {
       res.status(422).send(err);
-    }
-  });
-
-  app.get("/api/surveys/thanks", (req, res) => {
-    res.send("Thanks for voting!");
-  });
-
-  app.get("/test", async (req, res) => {
-    const survey = {
-      title: "my title",
-      subject: "my subject",
-      recipients: "guillermo.sanz.gs@gmail.com",
-      body: "Here is the body of the email",
-    };
-    try {
-      const response = await axios.post("/api/surveys", survey);
-      res.send("GOOD: " + response);
-    } catch (err) {
-      res.send(err);
     }
   });
 };
